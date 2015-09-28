aa2d117220547fece429665e9f98d5e6d5addc40
rkoesling1 <rkoesling1@studentmail.nait.ca>
2015-09-28 16:41:09 -0600

    Continuing with eRestaurant

--- eRestaurant Demo/eRestaurant.Framework/Entities/Item.cs
+++ eRestaurant Demo/eRestaurant.Framework/Entities/Item.cs
@@ -11,15 +11,28 @@ namespace eRestaurant.Framework.Entities
     {
         [Key]
         public int ItemID { get; set; }
+
+        [Required, StringLength(5)]
         public string Description { get; set; }
+
+        [Range(0.01, 5.00)]
         public double CurrentPrice { get; set; }
+
+        [Range(0.01, 30.00)]
         public double CurrentCost { get; set; }
         public bool Active { get; set; }
+
+        [Range(0, int.MaxValue)]
         public int? Category { get; set; }
         public string Comment { get; set; }
         public int MenuCategoryID { get; set; }
         
         //Navigation Properties
         public virtual MenuCategory MenuCategory { get; set; }
+
+        public Item()
+        {
+            Active = true;
+        }
     }
 }
