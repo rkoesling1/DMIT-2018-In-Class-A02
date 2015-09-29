e9bbe075879e285ffd354b24a45ca51d0045fb3e
rkoesling1 <rkoesling1@studentmail.nait.ca>
2015-09-29 15:46:16 -0600

    Created Bill, Waiter, and BillItem entities

--- eRestaurant Demo/eRestaurant.Framework/DAL/RestaurantContext.cs
+++ eRestaurant Demo/eRestaurant.Framework/DAL/RestaurantContext.cs
@@ -16,13 +16,16 @@ namespace eRestaurant.Framework.DAL
             : base("DefaultConnection")
         { }
         // One property for each Table/Entity in the database
+        // The property name must match the name of the database table
         public DbSet<MenuCategory> MenuCategories { get; set; }
         public DbSet<Item> Items { get; set; }
-        // The property name must match the name of the database table
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<BillItem> BillItems { get; set; }
         public DbSet<Table> Tables { get; set; }
+        public DbSet<Waiter> Waiters { get; set; }
         public DbSet<SpecialEvent> SpecialEvents { get; set; }
         public DbSet<Reservation> Reservations { get; set; }
-
+       
         // For customizing the model of our entities as we want them to match our
         // database, we would put any details inside the following method
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
