a971e1998a9401903a74c77d66a6486f211dd2a6
rkoesling1 <rkoesling1@studentmail.nait.ca>
2015-10-22 16:51:14 -0600

    backup

--- eRestaurant Demo/eRestaurant.Framework/BLL/ReservationsController.cs
+++ eRestaurant Demo/eRestaurant.Framework/BLL/ReservationsController.cs
@@ -7,6 +7,7 @@ using System.ComponentModel; // comes with Entity Framework
 using eRestaurant.Framework.DAL;
 using eRestaurant.Framework.Entities;
 using eRestaurant.Framework.Entities.POCOs;
+using eRestaurant.Framework.Entities.DTO;
 
 namespace eRestaurant.Framework.BLL
 {
@@ -29,6 +30,34 @@ namespace eRestaurant.Framework.BLL
             }
         }
 
-
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<DailyReservation> ListUpcomingReservations(string eventCode)
+        {
+            using (var context = new RestaurantContext())
+            {
+                var results = from eachRow in context.Reservations
+                              where eachRow.ReservationStatus == "B"
+                              // TBA - && eachRow has the correct EventCode...
+                              orderby eachRow.ReservationDate
+                              // select eachRow
+                              group eachRow by new { eachRow.ReservationDate.Month, eachRow.ReservationDate.Day }
+                                  into dailyReservation
+                                  select new DailyReservation() // Create a DTO class called DailyReservation
+                                  {
+                                      Month = dailyReservation.Key.Month,
+                                      Day = dailyReservation.Key.Day,
+                                      Reservations = from booking in dailyReservation
+                                                     select new Booking() // Create a Booking POCO class
+                                                     {
+                                                         Name = booking.CustomerName,
+                                                         Time = booking.ReservationDate.TimeOfDay,
+                                                         NumberInParty = booking.NumberInParty,
+                                                         Phone = booking.ContactPhone,
+                                                         Event = booking.SpecialEvent.Description
+                                                     }
+                                  };
+                return results.ToList();
+            }
+        }
     }
 }
