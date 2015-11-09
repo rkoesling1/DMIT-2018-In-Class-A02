ab177b37bd4d4281d83721d96f14358d6dfc8820
rkoesling1 <rkoesling1@studentmail.nait.ca>
2015-11-09 15:23:38 -0700

    Added FrontDesk and code to TempController

--- eRestaurant Demo/eRestaurant.Framework/BLL/TempController.cs
+++ eRestaurant Demo/eRestaurant.Framework/BLL/TempController.cs
@@ -2,17 +2,30 @@
 using eRestaurant.Framework.Entities; // Access the namespace for my entity classes
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace eRestaurant.Framework.BLL
 {
+    [DataObject]
     public class TempController
     {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public DateTime GetLastBillDateTime()
+        {
+            using (var context = new RestaurantContext())
+            {
+                var result = context.Bills.Max(x => x.BillDate);
+                return result;
+            }
+        }
+
+
         public List<MenuCategory> ListMenuCategories()
         {
-            using(var context = new RestaurantContext())
+            using (var context = new RestaurantContext())
             {
                 var data = from category in context.MenuCategories
                            select category;
@@ -20,6 +33,6 @@ namespace eRestaurant.Framework.BLL
             }
         }
 
-        
+
     }
 }
